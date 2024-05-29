1a4ad2bf69d96d778156e96322e4cd2077526e0a
ingelos <mailvaninge@gmail.com>
2024-05-29 14:29:40 +0200

    Initial setup

---
+++ src/main/java/com/example/techiteasyspringhomework/TechiteasySpringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.techiteasyspringhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechiteasySpringHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechiteasySpringHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techiteasyspringhomework/TechiteasySpringHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techiteasyspringhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechiteasySpringHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
