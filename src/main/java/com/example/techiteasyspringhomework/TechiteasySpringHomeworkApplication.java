5e497977a26fe77788e29c513b804c56abc10e4a
ingelos <mailvaninge@gmail.com>
2024-05-29 21:02:13 +0200

    homework controller

--- src/main/java/com/example/techiteasyspringhomework/TechiteasySpringHomeworkApplication.java
+++ src/main/java/com/example/techiteasyspringhomework/TechiteasySpringHomeworkApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TechiteasySpringHomeworkApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TechiteasySpringHomeworkApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TechiteasySpringHomeworkApplication.class, args);
+	}
 
 }
